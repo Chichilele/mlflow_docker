1b0395f8f773fd16e42fb596702adbf275cbf331
Gauthier Castro <gauthier.castro@gmail.com>
2021-06-25 15:24:24 +0100

    get minio credentials and URI from env file

--- start_server.sh
+++ start_server.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-export MLFLOW_S3_ENDPOINT_URL=http://192.168.0.58:9000
-export AWS_ACCESS_KEY_ID=user01
-export AWS_SECRET_ACCESS_KEY=password
-
 echo MLFLOW_S3_ENDPOINT_URL:    "${MLFLOW_S3_ENDPOINT_URL}"
 
 # . /data/mlflow/pyenv_mlflow/bin/activate
